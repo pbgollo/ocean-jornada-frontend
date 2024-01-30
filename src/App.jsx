ad197241fd836096cee6fd3ad7cc6fa76fb87df2
Pedro Gollo <130512644+pbgollo@users.noreply.github.com>
2024-01-30 14:52:06 +0000

    Criação da lista de cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,31 @@ import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
+
   const item1 = {
     name: 'Rick Sanchez',
     image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
   }
+
   const item2 = {
     name: 'Morty Smith',
     image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
   }
+  
   const item3 = {
     name: 'Summer Smith',
     image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
   }
 
+  const itens = [item1, item2, item3]
+
   return (
     <>
       <div className='cards'>
-        <Card item = {item1}/>
+        {/* <Card item = {item1}/>
         <Card item = {item2}/>
-        <Card item = {item3}/>
+        <Card item = {item3}/> */}
+        {itens.map(item => <Card item={item} />)}
       </div>
     </>
   )
