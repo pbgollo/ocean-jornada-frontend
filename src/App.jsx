85fc3799c5d8c23a389a46d85df2a821b94bc58f
Pedro Gollo <130512644+pbgollo@users.noreply.github.com>
2024-01-30 14:12:14 +0000

    Ajuste da posição dos cards

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,18 @@ function App() {
     name: 'Morty Smith',
     image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
   }
+  const item3 = {
+    name: 'Summer Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
+  }
 
   return (
     <>
-      <Card item = {item1}/>
-      <Card item = {item2}/>
+      <div className='cards'>
+        <Card item = {item1}/>
+        <Card item = {item2}/>
+        <Card item = {item3}/>
+      </div>
     </>
   )
 }
