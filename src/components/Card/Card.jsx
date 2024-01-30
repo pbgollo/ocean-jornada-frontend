e44d40eb6774609baf13cb9fafd513d35ec77838
Pedro Gollo <130512644+pbgollo@users.noreply.github.com>
2024-01-30 12:49:39 +0000

    Estilização das tags

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,9 +3,11 @@ function Card() {
         <>
           <div className="card">
             <h2>Rick Sanchez</h2>
-            <div className="tag">Status: Vivo</div>
-            <div className="tag">Espécie: Humana</div>
-            <div className="tag">Origem: Terra C-137</div>
+            <div className="tags">
+              <div className="tag">Status: Vivo</div>
+              <div className="tag">Espécie: Humana</div>
+              <div className="tag">Origem: Terra C-137</div>
+            </div>
             <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
           </div>
         </>
