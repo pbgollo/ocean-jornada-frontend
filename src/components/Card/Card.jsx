a766b96170f43e8a6eb8ae6bf0e443f41856ce1b
Pedro Gollo <130512644+pbgollo@users.noreply.github.com>
2024-01-30 13:40:56 +0000

    Criação de objetos para novos cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,20 @@
-function Card() {
-    return (
-        <>
-          <div className="card">
-            <h2>Rick Sanchez</h2>
-            <div className="tags">
-              <div className="tag">Status: Vivo</div>
-              <div className="tag">Espécie: Humana</div>
-              <div className="tag">Origem: Terra C-137</div>
-            </div>
-            <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+function Card(props) {
+
+  const item = props.item
+
+  return (
+      <>
+         <div className="card">
+          <h2>{item.name}</h2>
+          <div className="tags">
+            <div className="tag">Status: Vivo</div>
+            <div className="tag">Espécie: Humana</div>
+            <div className="tag">Origem: Terra C-137</div>
           </div>
-        </>
-      )   
+          <img src={item.image}/>
+        </div>
+      </>
+    )   
 }
 
 export default Card
